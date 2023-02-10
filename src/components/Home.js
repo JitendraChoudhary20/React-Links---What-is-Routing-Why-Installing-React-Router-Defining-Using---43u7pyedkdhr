6fdc1b68e4cf80e463c28bad6d0d98d0e7ec47e6
Jitendra Choudhary <jituchoudhary20@gmail.com>
2023-02-10 21:02:04 +0530

    check

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div id="home-div">
       <ul>
         <li>
+          <Link to='/'>
           <h2 id="home-item">Home</h2>
+          </Link>
         </li>
         <li>
+          <Link to='/topics'>
           <h2 id="topics-item">Topics</h2>
+          </Link>
         </li>
       </ul>
       <hr />
